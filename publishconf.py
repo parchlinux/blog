4c1569931b9294cdb1d3f5a01035c1d527fc23fb
hikari <94219809+metantesan@users.noreply.github.com>
2023-08-30 00:24:11 +0330

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://parchlinux.ir/blog"
+SITEURL = "https://blog.parchlinux.ir"
 RELATIVE_URLS = False
 
 # Feed Items
